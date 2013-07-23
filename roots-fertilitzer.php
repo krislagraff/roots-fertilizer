b961e5269fd7904e0ef9c92f7e7addab678bf1c8
Jonathan Stanley <jonathan@bristleconeweb.com>
2013-07-23 15:02:54 -0600

    documentation and roles

--- roots-fertilitzer.php
+++ roots-fertilitzer.php
@@ -14,7 +14,10 @@ add_action('widgets_init', 'rootsfertilizer_widgets_init');
 add_action('admin_init', 'rootsfertilizer_admin_init'); 
 
 /**
- * Plugin meta
+ * Add a documentation link to Git Hub for the plugin
+ * @param  [type] $links the plugin links to be filtered
+ * @param  [type] $file  the plugin to be filtered
+ * @return [type]        the filtered links
  */
 function rootsfertilizer_plugin_row_meta($links, $file) {
     $plugin = plugin_basename(__FILE__);
@@ -28,7 +31,8 @@ function rootsfertilizer_plugin_row_meta($links, $file) {
 }
  
 /**
- * Register sidebars and widgets
+ * Initialize widgits
+ * @return [type] [description]
  */
 function rootsfertilizer_widgets_init() {
   // Create header widget
@@ -42,31 +46,59 @@ function rootsfertilizer_widgets_init() {
   ));
 }
 
-
+/**
+ * Run functions for admin
+ * @return [type] [description]
+ */
 function rootsfertilizer_admin_init() {
+  rootsfertilizer_update_options();
+  rootsfertilizer_file_permissions();
+  rootsfertilizer_theme_caps();
+}
+
   /**
    * Standard image sizes adjusted for standard bootstrap widths
    * Thumbnails are span2xspan2
    * Medium is span3 width
    * Large is span4 width
    * Upload full resolution for most images.  Images that will be shown at full resolution should be properly resized before uploading
+   * @return [type] [description]
    */
-  update_option('thumbnail_size_w', 170);
-  update_option('thumbnail_size_h', 170);
-  update_option('medium_size_w', 270);
-  update_option('medium_size_h', 9999);
-  update_option('large_size_w', 370);
-  update_option('large_size_h', 9999);
+  function rootsfertilizer_update_options(){
+    update_option('thumbnail_size_w', 170);
+    update_option('thumbnail_size_h', 170);
+    update_option('medium_size_w', 270);
+    update_option('medium_size_h', 9999);
+    update_option('large_size_w', 370);
+    update_option('large_size_h', 9999);
+  }
+
+  /**
+   * Allow contributors to view form entries
+   * @return [type] [description]
+   */
+  function rootsfertilizer_theme_caps() {
+      $role = get_role( 'contributor');
+      $role->add_cap( 'gravityforms_view_entries'); 
+      $role->add_cap( 'gravityforms_edit_entries'); 
+
+      $role->add_cap( 'gravityforms_view_entry_notes'); 
+      $role->add_cap( 'gravityforms_edit_entry_notes');
+
+      $role->add_cap( 'gravityforms_export_entries'); 
+  }
 
   /**
    * Remove extra menus / maintain source control / disble access to file changes when on production server
+   * @return [type] [description]
    */
-  if (!strstr(site_url(),'localhost')){
-    define('DISALLOW_FILE_EDIT', true);
-    define('DISALLOW_FILE_MODS', true);
-    //perhaps define('FORCE_SSL_ADMIN',true);
+  function rootsfertilizer_file_permissions(){
+    if (!strstr(site_url(),'localhost')){
+      define('DISALLOW_FILE_EDIT', true);
+      define('DISALLOW_FILE_MODS', true);
+      //perhaps define('FORCE_SSL_ADMIN',true);
+    }
   }
-}
 
 
 /**
@@ -76,6 +108,7 @@ function rootsfertilizer_admin_init() {
 
 /**
  * see http://codex.wordpress.org/Conditional_Tags#Testing_for_sub-Pages snippet 2
+ * @return boolean [description]
  */
 function is_subpage() {
     global $post;
@@ -88,6 +121,8 @@ function is_subpage() {
 
 /**
  * see http://codex.wordpress.org/Conditional_Tags#Testing_for_sub-Pages snippet 4
+ * @param  [type]  $pid the id of the post to test
+ * @return boolean      true if $pid is an ancestor of the current post.  Otherwise returnse false.
  */
 function is_tree( $pid ) {
     global $post;
@@ -104,6 +139,7 @@ function is_tree( $pid ) {
 
 /**
  * determine if current page has child pages
+ * @return boolean true if current post has children
  */
 function has_children(){
   global $post;
